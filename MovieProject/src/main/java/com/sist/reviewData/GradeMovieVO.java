790ef73cb1c547733785027c51ebe5ff8471c7a7
inhye-choi <choiinhye25@gmail.com>
2018-07-04 11:37:45 +0900

    Main Page UI updated

---
+++ MovieProject/src/main/java/com/sist/reviewData/GradeMovieVO.java
@@ -0,0 +1,47 @@
+package com.sist.reviewData;
+public class GradeMovieVO {
+	 private String code;//영화코드
+	 private String title;//영화제목
+	 private int grno;//평점 번호
+	 private String grcontent;//평점 내용
+	 private double grscore;//평점 평점
+	 private String grmon;//평점 작성 달
+	 public String getTitle() {
+	  return title;
+	  
+	 }
+	 public void setTitle(String title) {
+	  this.title = title;
+	 }
+	 public String getCode() {
+	  return code;
+	 }
+	 public void setCode(String code) {
+	  this.code = code;
+	 }
+	public int getGrno() {
+		return grno;
+	}
+	public void setGrno(int grno) {
+		this.grno = grno;
+	}
+	public String getGrcontent() {
+		return grcontent;
+	}
+	public void setGrcontent(String grcontent) {
+		this.grcontent = grcontent;
+	}
+	public double getGrscore() {
+		return grscore;
+	}
+	public void setGrscore(double grscore) {
+		this.grscore = grscore;
+	}
+	public String getGrmon() {
+		return grmon;
+	}
+	public void setGrmon(String grmon) {
+		this.grmon = grmon;
+	}
+	
+	}
---
+++ MovieProject/src/main/java/com/sist/reviewData/ReviewMovieVO.java
@@ -0,0 +1,40 @@
+package com.sist.reviewData;
+
+public class ReviewMovieVO {	
+	private String code;
+	private String title;
+	private int rvno;//리뷰 번호
+	private String rvtitle;//리뷰 제목
+	private String rvcontent;//리뷰 내용 
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public int getRvno() {
+		return rvno;
+	}
+	public void setRvno(int rvno) {
+		this.rvno = rvno;
+	}
+	public String getRvtitle() {
+		return rvtitle;
+	}
+	public void setRvtitle(String rvtitle) {
+		this.rvtitle = rvtitle;
+	}
+	public String getRvcontent() {
+		return rvcontent;
+	}
+	public void setRvcontent(String rvcontent) {
+		this.rvcontent = rvcontent;
+	}
+		
+}
